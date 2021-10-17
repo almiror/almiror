7c997b1d4da7c5655247a98930af32278f041eb0
Julio Quiñones <obtenerrecursos@gmail.com>
2021-10-17 11:54:42 -0500

    INIT PROYECT 2022

---
+++ docs/configurations/minimali.js
@@ -0,0 +1,4 @@
+export const iD = document.getElementById.bind(document);
+export const qS = document.querySelector.bind(document);
+export const qSA = document.querySelectorAll.bind(document);
+export {iD,qS,qSA}
---
+++ src/js/menu.main.js
@@ -0,0 +1,5 @@
+import {iD,qS,qSA} from "../../docs/configurations/minimali.js"
+
+console.log(iD)
+console.log(qS)
+console.log(qSA)
