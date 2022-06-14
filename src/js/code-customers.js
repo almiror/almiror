313489071a20acdbcf5a3e2f6b74ab84e510bac2
Julio <almiror.info@gmail.com>
2022-06-14 17:07:10 -0500

    EL MODULO CAJA FINALIZADO

--- src/js/code-customers.js
+++ src/js/code-customers.js
@@ -370,11 +370,11 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left">
                                     LAS VENTAS DEL AREA DE SERVICIO SEAN REFLEJADOS EN EL CIERRE DE CAJA
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                    PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-warning">
-                                PROCESO 5%
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
                             </tr>                           
                             <tr class="smaller s-border-1">
