3435c05ed5659d4acfbfc98aad647d9bde8350c8
Julio <almiror.info@gmail.com>
2022-06-15 08:22:47 -0500

    NUEVOS ESTADO EN LA TABLA DE ACTUALIZACIONES DEL SISTEMA

--- src/js/code-customers.js
+++ src/js/code-customers.js
@@ -307,8 +307,8 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left color-danger">
                                     PENDIENTE
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                    PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-warning">
+                                PROCESO 40%
                                 </td>
                             </tr>                           
                             <tr class="smaller s-border-1">
@@ -340,8 +340,8 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left color-danger">
                                     PENDIENTE
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                    PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-warning">
+                                    PROCESO 10%
                                 </td>
                             </tr>                           
                             <tr class="smaller s-border-1">
