7443a5d602396d0b931a3a3c0daa7c05a4272c68
Julio <almiror.info@gmail.com>
2022-06-28 16:17:42 -0500

    ❤️

--- src/js/code-customers.js
+++ src/js/code-customers.js
@@ -441,11 +441,11 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left">
                                 AGREGAR EL CAMPO IMAGEN EN LA SECCION (ALMACEN PRODUCTOS)
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-warning">
-                                PROCESO 50%
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
                             </tr>                      
                             <tr class="smaller s-border-1">
@@ -459,7 +459,7 @@ const appendBody  = ()=> {
                                 PENDIENTE
                                 </td>
                                 <td class="s-pxy-2 s-text-left color-danger">
-                                PENDIENTE
+                                PROCESO 50%
                                 </td>
                             </tr>                      
                             <tr class="smaller s-border-1">
