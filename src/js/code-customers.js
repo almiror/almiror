87ea1984267c58740aca2ae038f4e4891a7cc706
Julio <almiror.info@gmail.com>
2022-07-07 16:14:56 -0500

    ❤️

--- src/js/code-customers.js
+++ src/js/code-customers.js
@@ -571,11 +571,11 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left">
                                 REESTRUCTURACION GENEREAL DE CUENTAS POR COBRAR (listado tipo cuentas por pagar)
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-warning">
-                                PROCESO 50%
+                                <td class="s-pxy-2 s-text-left color-success">
+                                FINALIZADO
                                 </td>
                             </tr>                       
                             <tr class="smaller s-border-1">
@@ -630,8 +630,8 @@ const appendBody  = ()=> {
                                 <td class="s-pxy-2 s-text-left color-danger">
                                 PENDIENTE
                                 </td>
-                                <td class="s-pxy-2 s-text-left color-danger">
-                                PENDIENTE
+                                <td class="s-pxy-2 s-text-left color-warning">
+                                PROCESO 05%
                                 </td>
                             </tr>  
                             <tr class="smaller s-border-1">      
